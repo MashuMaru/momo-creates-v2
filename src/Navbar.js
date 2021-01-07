8f703b54ae4e673e13ac8bc20417b30fcf256d22
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-07 08:31:56 +0000

    created MOMO CREATES - V2

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navbar() {
+    return <div className="navbar">
+        <a className="nav-link" href="#home">home</a>
+        <a className="nav-link" href="#about">about</a>
+        <a className="nav-link" href="#work">work</a>
+        <a className="nav-link" href="#contact">contact</a>
+    </div>
+}
