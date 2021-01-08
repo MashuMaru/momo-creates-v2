f664d2bf7195bd40656eed47a901aecc08644fc2
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-08 13:50:50 +0000

    minor page amendments

--- src/About.js
+++ src/About.js
@@ -1,44 +1,30 @@
 import React from 'react';
 import Profile from './images/profile.png';
+import AOS from 'aos';
 
 function About() {
+    AOS.init();
     return <div className="about-section" id="about">
-            {/* <div className="about-text-reveal">
-                <h2 className="about-title-reveal">about me</h2>
-                <img className="about-image-reveal" src={Profile} alt="profile"></img>
-                <p className="about-paragraph-reveal">Add a Lorem Ipsum is simply dummy,
-                 text of the printing and typesetting industry.</p>
-                 <p className="about-paragraph-reveal">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                 <p className="about-paragraph-reveal">It has survived not only five centuries, but also the leap into electronic typesetting, 
-                 remaining essentially unchanged. 
-                 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
-                 bit of body text</p>
 
-            </div> */}
+        <div className="about-left-section" data-aos="fade-right" data-aos-duration="1000">
+                <div className="about-text">
+                    <h2 className="about-title">about me</h2>
+                    <img className="about-image-reveal" src={Profile} alt="profile"></img>
+                    <p className="about-paragraph">Add a Lorem Ipsum is simply dummy,
+                    text of the printing and typesetting industry.</p>
+                    <p className="about-paragraph">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    <p className="about-paragraph">It has survived not only five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. 
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                    bit of body text</p>
 
-        <div className="about-left-section">
-        <div className="about-text">
-                <h2 className="about-title">about me</h2>
-                <img className="about-image-reveal" src={Profile} alt="profile"></img>
-                <p className="about-paragraph">Add a Lorem Ipsum is simply dummy,
-                 text of the printing and typesetting industry.</p>
-                 <p className="about-paragraph">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                 <p className="about-paragraph">It has survived not only five centuries, but also the leap into electronic typesetting, 
-                 remaining essentially unchanged. 
-                 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
-                 bit of body text</p>
-
-            </div>
+                </div>
         </div>
 
-        <div className="about-right-section">
-        {/* <div className="landing-img-box"> */}
-        <img className="about-image" src={Profile} alt="profile"></img>
-            {/* </div> */}
+        <div className="about-right-section" data-aos="fade-left" data-aos-duration="1000">
+            <img className="about-image" src={Profile} alt="profile"></img>
         </div>
     </div>
 }
--- src/Landing.js
+++ src/Landing.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Main from './images/momo-closeup-landing.png';
+import AOS from 'aos';
 
 function Landing() {
+    AOS.init();
     return <div className="landing-section" id="home">
-
-        <div className="land-left-section">
-            {/* <div className="landing-img-box"> */}
+        <div className="land-left-section" data-aos="zoom-out-down" data-aos-duration="2000">
                 <img className="main-image" src={Main} alt="landing"></img>
-            {/* </div> */}
         </div>
 
-        <div className="land-right-section">
+        <div className="land-right-section" data-aos="zoom-out-down" data-aos-duration="2000">
             <div className="landing-text">
                 <h1 className="header-title">MOMO CREATES</h1>
                 <p className="header-sub-title">THE EMBROIDERY TO YOUR DREAMS</p>
             </div>
         </div>
+
     </div>
 }
 
